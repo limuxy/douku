3dc5426722918fc67d5b2fe80a4bf09f658410e4
limuxy <limuxy@gmail.com>
2015-07-24 15:44:37 +0800

    correction on config importing

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('../config.py')
 
 if app.config['MONGO_USER'] != "":
     mongoURI = 'mongodb://{}:{}@{}'.format(app.config['MONGO_USER'], app.config['MONGO_PWD'], app.config['MONGO_IP'])
